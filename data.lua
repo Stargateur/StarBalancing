0a6dd01194f156f40c259d5cdaf9e74555c96d29
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2025-10-24 17:10:45 +0200

    0.3.0

--- data.lua
+++ data.lua
@@ -61,3 +61,12 @@ end
 data.raw["item"]["uranium-ore"].weight = 1000 * kg / 500
 data.raw["item"]["uranium-235"].weight = 1000 * kg / 100
 data.raw["item"]["uranium-238"].weight = 1000 * kg / 100
+
+local ratio = 3
+local plde = data.raw["active-defense-equipment"]["personal-laser-defense-equipment"]
+
+if plde then
+    plde.attack_parameters.damage_modifier = ratio
+    plde.attack_parameters.ammo_type.energy_consumption = 400 * ratio .. "kJ"
+    plde.energy_source.buffer_capacity = 400 * ratio * 10 .. "kJ"
+end
